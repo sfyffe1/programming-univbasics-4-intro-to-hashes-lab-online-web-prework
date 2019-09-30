817b722c4805e68847356ade9f240bd5a576c3f3
Sean Fyffe <stfyffe@gmail.com>
2019-09-30 17:00:53 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,11 +15,14 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  
+  hash = {
+    key => value
+  }
+  hash
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)
