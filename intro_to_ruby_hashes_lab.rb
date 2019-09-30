8b10fd6e00bde3b78f858d036986327c98d307dc
Sean Fyffe <stfyffe@gmail.com>
2019-09-30 18:29:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ end
 def update_counting_hash(hash, key)
   hash[key]
   if key[hash] update_hash{
-    :apples => 3
+    :key => +1
   }
-  
+end
 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
